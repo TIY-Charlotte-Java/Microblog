8c2c49a516e34017df49264e3e7408e76785fc4f
Emileen Marianayagam <emileenmarianayagam@Emileens-MacBook-Pro.local>
2016-12-17 18:08:26 -0500

    work in progress

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -6,29 +6,44 @@ import spark.template.mustache.MustacheTemplateEngine;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Main {
     static User user;
-    static Message message;
+
 
     public static void main(String[] args) {
         // write your code here
         Spark.init();
 
+
         Spark.get("/", ((request, response) -> {
                     HashMap<String, String> home = new HashMap();
-                    ArrayList<Message> message = new ArrayList<Message>();
-
                     if (user == null) {
                         return new ModelAndView(home, "index.html");
                     } else {
                         home.put("name", user.name);
-                        return new ModelAndView(message, "message.html");
-                        message.add(0,message.set());
+                        return new ModelAndView(home, "message.html");
+
                     }
 
                 }),
                 new MustacheTemplateEngine());
+
+
+        Spark.get("/postMessage", (((request, response) -> {
+            HashMap<String, ArrayList<String>> messageStore = new HashMap<>();
+
+            if (user.message == null) {
+                return new ModelAndView(messageStore, "message.html");
+            } else {
+                messageStore.put("message", user.message);
+                return new ModelAndView(messageStore, "message.html");
+
+
+            }
+
+        })), new MustacheTemplateEngine());
 
 
         Spark.post("/login",
@@ -40,10 +55,17 @@ public class Main {
 
                 })
 
+
         );
 
-    }
+        Spark.post("/postMessage", (((request, response) -> {
+            String message = request.queryParams("Message");
+            user = new User(message);
+            response.redirect("/postMessage");
+            return "";
+        })));
 
+    }
 
 
 }
--- src/com/theironyard/charlotte/User.java
+++ src/com/theironyard/charlotte/User.java
@@ -1,13 +1,19 @@
 package com.theironyard.charlotte;
 
+import java.util.ArrayList;
+
 /**
  * Created by emileenmarianayagam on 12/15/16.
  */
 public class User {
     String name;
+    ArrayList<String> message;
+
+
 
     public User(String name) {
         this.name = name;
+        this.message = new ArrayList<>();
     }
 
 
