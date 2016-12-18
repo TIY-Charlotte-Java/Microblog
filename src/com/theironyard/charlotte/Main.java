8d077699c3c63044c293da7d685cf32ccdb395d4
Kelsey Newman <kpnewman2013@gmail.com>
2016-12-18 18:15:03 -0500

    regular mode

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,8 +1,47 @@
 package com.theironyard.charlotte;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
 public class Main {
+    public static User user;
 
     public static void main(String[] args) {
-	// write your code here
+        Spark.get("/",
+                ((request, response) -> {
+                    HashMap<String, Object> userNames = new HashMap<>();
+                 if (user == null) {
+                     return new ModelAndView(userNames, "index.html");
+                 } else {
+                     userNames.put("name" , user.name);
+                     userNames.put("message", user.storeMessage);
+                     return new ModelAndView(userNames, "messages.html");
+                 }
+                }),
+                new MustacheTemplateEngine()
+
+        );
+        Spark.post("/create-user",
+                ((request, response) -> {
+                    String name = request.queryParams("loginName");
+                    user = new User(name, null);
+                    response.redirect("/");
+                    return "";
+                })
+                );
+
+        Spark.post("/create-message",
+                ( request, response) -> {
+                    String message = request.queryParams("message");
+                    user.storeMessage.add(message);
+                    response.redirect("/");
+                    return "";
+                }
+        );
+
+
     }
 }
