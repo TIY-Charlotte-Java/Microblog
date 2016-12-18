306d48c12b4c54ab0c11e99debf8982da95e53c8
Joe Langley <josephelangley@gmail.com>
2016-12-18 16:20:34 -0500

    Isn't updating comments to the website

---
+++ src/com/theironyard/charlotte/User.java
@@ -0,0 +1,12 @@
+package com.theironyard.charlotte;
+
+/**
+ * Created by Joe on 12/17/16.
+ */
+public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
