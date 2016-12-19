f3f3423d89ccfb6fa31677c5f1e622ad237ef731
Emileen Marianayagam <emileenmarianayagam@Emileens-MacBook-Pro.local>
2016-12-18 21:01:04 -0500

    changes made

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -6,44 +6,54 @@ import spark.template.mustache.MustacheTemplateEngine;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Main {
     static User user;
-    static Message message;
+    //static Message message;
+
 
     public static void main(String[] args) {
         // write your code here
         Spark.init();
 
-        Spark.get("/", ((request, response) -> {
-                    HashMap<String, String> home = new HashMap();
-                    ArrayList<Message> message = new ArrayList<Message>();
 
+        Spark.get("/", ((request, response) -> {
+            //creating a HashMap to store a name and message
+                    HashMap home = new HashMap();
                     if (user == null) {
                         return new ModelAndView(home, "index.html");
                     } else {
                         home.put("name", user.name);
-                        return new ModelAndView(message, "message.html");
-                        message.add(0,message.set());
+                        home.put("messages", user.messages);
+                        return new ModelAndView(home, "message.html");
                     }
-
                 }),
                 new MustacheTemplateEngine());
 
 
         Spark.post("/login",
                 ((request, response) -> {
+                    // we're getting the name from the form the user submitted
                     String name = request.queryParams("loginName");
-                    user = new User(name);
+                    //using that name to set the current user and the message
+                    user = new User(name, null);
+                    //go back to the homepage
                     response.redirect("/");
                     return "";
-
                 })
+
 
         );
 
-    }
+        Spark.post("/postMessage", (((request, response) -> {
+            String message = request.queryParams("message");
+            user.messages.add(message);
+            response.redirect("/");
+            return "";
+        })));
 
+    }
 
 
 }
--- src/com/theironyard/charlotte/User.java
+++ src/com/theironyard/charlotte/User.java
@@ -1,16 +1,19 @@
 package com.theironyard.charlotte;
 
+import java.util.ArrayList;
+
 /**
  * Created by emileenmarianayagam on 12/15/16.
  */
 public class User {
+
     String name;
+    ArrayList<String> messages;
 
-    public User(String name) {
+    public User(String name, ArrayList<String> messages) {
         this.name = name;
+        this.messages = new ArrayList<>();
     }
-
-
 
 
 }
