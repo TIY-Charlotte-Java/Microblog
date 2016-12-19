d50e61d595dd5e4b6822d5bd7d52a6c336342ff8
Emileen Marianayagam <emileenmarianayagam@Emileens-MacBook-Pro.local>
2016-12-19 09:06:07 -0500

    Adding the .. working code

--- src/com/theironyard/charlotte/User.java
+++ src/com/theironyard/charlotte/User.java
@@ -6,26 +6,21 @@ import java.util.ArrayList;
  * Created by emileenmarianayagam on 12/15/16.
  */
 public class User {
-<<<<<<< HEAD
-=======
+
     String name;
-    ArrayList<String> message;
-
-
->>>>>>> 8c2c49a516e34017df49264e3e7408e76785fc4f
+    ArrayList<String> messages;
 
     public User(String name, ArrayList<String> messages) {
         this.name = name;
         this.messages = new ArrayList<>();
     }
 
-    public String getName() {
+    /*public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-        this.message = new ArrayList<>();
     }
 
     public ArrayList<String> getMessages() {
@@ -35,9 +30,8 @@ public class User {
     public void setMessages(ArrayList<String> messages) {
         this.messages = messages;
     }
+*/
 
-    String name;
-    ArrayList<String> messages;
 
 
 
