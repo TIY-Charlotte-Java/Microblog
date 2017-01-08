76bbb33a8ee1b1af44b23cf3b9d58e73ffd3dc68
Stephen Williamson <stephenwilliamson@Stephens-MacBook.local>
2017-01-08 15:52:37 -0500

    No Trump tweets

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,8 +1,57 @@
 package com.theironyard.charlotte;
 
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 public class Main {
 
+    public static User user;
+
+
     public static void main(String[] args) {
-	// write your code here
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    HashMap<String, Object> m = new HashMap<>();
+                    if (user == null) {
+                        return new ModelAndView(m, "index.html");
+                    }
+                    else{
+                        m.put("name", user.name);
+                        m.put("messages",user.storeMessage);
+                        return new ModelAndView(m, "messages.html");
+                    }
+                }),
+                new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/create-user",
+                ((request, response) -> {
+                    String name = request.queryParams("userName");
+                    ArrayList<String> messages = new ArrayList<String>();
+                    user = new User(name,messages);
+
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.post(
+                "/create-message",
+                ((request, response) -> {
+                    String message = request.queryParams("message");
+                    if (message == null || "".equals(message)){
+                    }
+                    else {
+                        user.storeMessage.add(message);
+                    }
+                    response.redirect("/");
+                    return "";
+                })
+        );
     }
 }
